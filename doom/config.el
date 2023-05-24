7f8d626975394609cc02f251c6b6afa067b17973
MadFisa <asifmp97@gmail.com>
2023-05-24 22:09:18 +0530

    feat: Added new TODO keywords and autoformat

--- doom/config.el
+++ doom/config.el
@@ -111,6 +111,44 @@
          :clock-resume t
          :empty-lines 0)
         )
+
+(setq org-todo-keywords
+'((sequence
+        "TODO(t)"  ; A task that needs doing & is ready to do
+        "PROJ(p)"  ; A project, which usually contains other tasks
+        "LOOP(r)"  ; A recurring task
+        "STRT(s)"  ; A task that is in progress
+        "WAIT(w)"  ; Something external is holding up this task
+        "HOLD(h)"  ; This task is paused/on hold because of me
+        "QUESTION(q)"  ; This task is paused/on hold because of me
+        "BUG(b)"  ; Bugs to squash
+        "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+        "|"
+        "DONE(d)"  ; Task successfully completed
+        "ANSWERED(a)"  ; This task is paused/on hold because of me
+        "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+        (sequence
+        "[ ](T)"   ; A task that needs doing
+        "[-](S)"   ; Task is in progress
+        "[?](W)"   ; Task is being held up or paused
+        "|"
+        "[X](D)")  ; Task was completed
+        (sequence
+        "|"
+        "OKAY(o)"
+        "YES(y)"
+        "NO(n)"))
+org-todo-keyword-faces
+'(("[-]"  . +org-todo-active)
+        ("STRT" . +org-todo-active)
+        ("[?]"  . +org-todo-onhold)
+        ("WAIT" . +org-todo-onhold)
+        ("HOLD" . +org-todo-onhold)
+        ("PROJ" . +org-todo-project)
+        ("NO"   . +org-todo-cancel)
+        ("BUG"   . +org-todo-cancel)
+        ("QUESTION"   . +org-todo-cancel)
+        ("KILL" . +org-todo-cancel)))
   )
 (setq-default
  delete-by-moving-to-trash t )
